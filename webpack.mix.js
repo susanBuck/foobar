26dbd10921c75b6ac0cbeaec49ba11776d9ae74c
Susan Buck <susan@susanbuck.net>
2018-01-19 13:03:47 -0500

    Simplify resources/less directory structure to narrow down potential issues

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,18 @@
 let mix = require('laravel-mix');
 
-mix.less('resources/less/app.less', 'css/')  // 1
-    .less('resources/less/auth/auth.less', 'css/auth/') // 2
-    .less('resources/less/auth/login.less', 'css/auth/') // 3
+mix.less('resources/less/a.less', 'css/')
+    .less('resources/less/b.less', 'css/')
+    .less('resources/less/c.less', 'css/')
+    .less('resources/less/d.less', 'css/')
 
-    .less('resources/less/groups/create.less', 'css/groups/') // 4
-    .less('resources/less/groups/show.less', 'css/groups/') // 5
-    .less('resources/less/groups/edit.less', 'css/groups/') // 6
-    .less('resources/less/groups/index.less', 'css/groups/') // 7
+    .less('resources/less/e.less', 'css/')
+    .less('resources/less/f.less', 'css/')
+    .less('resources/less/g.less', 'css/')
+    .less('resources/less/h.less', 'css/')
 
-    .less('resources/less/home/guest.less', 'css/home/') // 8
-    .less('resources/less/home/user.less', 'css/home/') // 9
-
-    .less('resources/less/responses/create.less', 'css/responses/') // 10
-    .less('resources/less/responses/done.less', 'css/responses/') // 11
-    .less('resources/less/responses/notSupported.less', 'css/responses/'); // 12
-
-
+    .less('resources/less/i.less', 'css/')
+    .less('resources/less/j.less', 'css/')
+    .less('resources/less/k.less', 'css/')
+    .less('resources/less/l.less', 'css/');
 
 
